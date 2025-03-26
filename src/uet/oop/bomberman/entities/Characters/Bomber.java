55d18cfae0daeb3f34558ed41851b46e06c8e5c1
Do Tien Dat <dotiendat711@gmail.com>
2025-03-27 00:53:39 +0700

    feat: Add class Bomber in uet.oop.bomberman.entities.Characters

--- src/uet/oop/bomberman/entities/Characters/Bomber.java
+++ src/uet/oop/bomberman/entities/Characters/Bomber.java
@@ -1,19 +1,7 @@
-package uet.oop.bomberman.entities;
+package uet.oop.bomberman.entities.Characters;
 
-import javafx.scene.SnapshotParameters;
-import javafx.scene.canvas.GraphicsContext;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
-import javafx.scene.paint.Color;
+import uet.oop.bomberman.entities.Entity;
 
-public class Bomber extends Entity {
+public class Bomber {
 
-    public Bomber(int x, int y, Image img) {
-        super( x, y, img);
-    }
-
-    @Override
-    public void update() {
-
-    }
 }
