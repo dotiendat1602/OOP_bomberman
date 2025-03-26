848aa77c74859a0a70fa9c904980048fa2b551c2
Do Tien Dat <dotiendat711@gmail.com>
2025-03-26 20:01:03 +0700

    Bổ sung class Board

--- src/uet/oop/bomberman/Board.java
+++ src/uet/oop/bomberman/Board.java
@@ -1,2 +1,36 @@
-package uet.oop.bomberman;public class Board {
+package uet.oop.bomberman;
+
+import uet.oop.bomberman.entities.Entity;
+import uet.oop.bomberman.graphics.Screen;
+import uet.oop.bomberman.Input.Keyboard;
+
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class Board {
+    protected Game game;
+    protected Keyboard input;
+    protected Screen screen;
+
+    public Entity[] entities;
+
+    private int width;
+    private int height;
+
+
+    public Board(Game game, Keyboard input, Screen screen) {
+        this.game = game;
+        this.input = input;
+        this.screen = screen;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
 }
