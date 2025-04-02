9ecf3232d5fd682deb0bed79956f7990e108a55c
minductran <minhductran042@gmail.com>
2025-04-02 22:37:15 +0700

    :feat: Cac thuoc tinh co ban cua Bomber

--- src/uet/oop/bomberman/entities/Characters/Bomber.java
+++ src/uet/oop/bomberman/entities/Characters/Bomber.java
@@ -1,7 +1,28 @@
 package uet.oop.bomberman.entities.Characters;
 
+import uet.oop.bomberman.Board;
 import uet.oop.bomberman.entities.Entity;
+import uet.oop.bomberman.entities.Tile.*;
+import uet.oop.bomberman.Game;
+import uet.oop.bomberman.graphics.Screen;
+import uet.oop.bomberman.graphics.Sprite;
+import uet.oop.bomberman.Input.Keyboard;
+import uet.oop.bomberman.Level.Coordinates;
 
-public class Bomber {
+import java.awt.*;
+import java.util.Iterator;
+import java.util.List;
+
+
+public class Bomber extends Character{
+    private final int time = 15;
+    protected Keyboard input;
+    protected int finalAnimation = 30;
+
+    private int countTime = 0;
+    private boolean render = true;
+    private int direction;
+    private final int maxSteps;
+    private int steps;
 
 }
